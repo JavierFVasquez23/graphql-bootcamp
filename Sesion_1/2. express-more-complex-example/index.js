05b1ed75bea4986cabb9bf100fbba895e3cf0b70
JavierFVasquez23 <javier.vasquez@grability.com>
2020-02-11 14:57:38 -0500

    Sesion_1

---
+++ Sesion_1/2. express-more-complex-example/index.js
@@ -0,0 +1,68 @@
+var express = require("express");
+var graphqlHTTP = require("express-graphql");
+var { buildSchema } = require("graphql");
+var crypto = require("crypto");
+var schemaModel = require("./schema.graphql");
+
+var schema = buildSchema(schemaModel);
+
+class Message {
+  constructor(id, { content, author }) {
+    this.id = id;
+    this.content = content;
+    this.author = author;
+  }
+}
+
+// Maps username to content
+var fakeDatabase = {};
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  quoteOfTheDay: () => {
+    return Math.random() < 0.5 ? "Take it easy" : "Salvation lies within";
+  },
+  random: () => {
+    return Math.random();
+  },
+  rollDice: ({ numDice, numSides }) => {
+    var output = [];
+    for (var i = 0; i < numDice; i++) {
+      output.push(1 + Math.floor(Math.random() * (numSides || 6)));
+    }
+    return output;
+  },
+  getMessage: ({ id }) => {
+    if (!fakeDatabase[id]) {
+      throw new Error("no message exists with id " + id);
+    }
+    return new Message(id, fakeDatabase[id]);
+  },
+  createMessage: ({ input }) => {
+    // Create a random id for our "database".
+    var id = crypto.randomBytes(10).toString("hex");
+
+    fakeDatabase[id] = input;
+    return new Message(id, input);
+  },
+  updateMessage: ({ id, input }) => {
+    if (!fakeDatabase[id]) {
+      throw new Error("no message exists with id " + id);
+    }
+    // This replaces all old data, but some apps might want partial update.
+    fakeDatabase[id] = input;
+    return new Message(id, input);
+  }
+};
+
+var app = express();
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+  })
+);
+app.listen(4000);
+console.log("Running a GraphQL API server at localhost:4000/graphql");
